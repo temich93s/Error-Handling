f978a18e449494ac67b3a573969a377cc1dd907a
Temich93s <nil>
2021-10-07 13:06:13 +1000

    Пройдена тема: Отображение и генерация ошибок

--- Error Handling/main.swift
+++ Error Handling/main.swift
@@ -9,3 +9,14 @@ import Foundation
 
 print("Hello, World!")
 
+
+//MARK: Отображение и генерация ошибок
+print("\n//Отображение и генерация ошибок")
+
+enum VendingMachineError: Error {
+    case invalidSelection
+    case insufficientFunds(coinsNeeded: Int)
+    case outOfStock
+}
+
+//throw VendingMachineError.insufficientFunds(coinsNeeded: 5)
